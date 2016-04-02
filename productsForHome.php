c68e5e94f38df9c91bba3819a679baa42b1e0083
Harryki <zgp7777@gmail.com>
2016-04-02 02:04:07 -0700

    hid model number of flower and price label

--- productsForHome.php
+++ productsForHome.php
@@ -5,33 +5,35 @@ loadScripts();
 
 $data = array("status" => "not set!");
 
-if(Utils::isGET()) {
+if (Utils::isGET()) {
     $pm = new ProductManager();
     $rows = $pm->listProducts();
 
     $html = "";
-    if(!$rows || count($rows) == 0) {
+    if (!$rows || count($rows) == 0) {
         $html = "<tr><td>There are no results.</td></tr>";
     } else {
 
-        foreach($rows as $row) {
-			$path = $row['path'];
+        foreach ($rows as $row) {
+            $path = $row['path'];
             $sku = $row['SKU'];
             $price = $row['item_price'];
             $desc = $row['description'];
-			
 
-			$html .= "		<div class='col-md-4'>
+
+            $html .= "		<div class='col-md-4 thumbnailwrapper' >
 			<a href='#' class='thumbnail'>
 				 <img src=$path alt=$desc style='width:150px;height:150px'>
+				 <br/>
 				<p data-sku-desc='$sku'>$desc</p>
+				<!--
 				<p>model# $sku</p>
-				<strong>Prcie: $</strong><strong data-sku-price='$sku'>$price</strong>
+				-->
+				<strong>$</strong><strong data-sku-price='$sku'>$price</strong>
 			</a>			
 			<input data-sku-add='$sku' type='button' value='Add to cart' class='btn btn-primary'/>
 		</div>";
-            			
-			
+
 
         }
 
