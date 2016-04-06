6fbcddff9f988a7f4c0c23986d38e6410ae62184
Viktor <purple_goth@bk.ru>
2016-04-05 21:19:09 -0700

    Revert "commenting carousel"
    This reverts commit a67a2c43c6a213d4d317356c82ec41360d7538e7.

--- homepage.php
+++ homepage.php
@@ -7,49 +7,47 @@ require 'aboutProduct.php';
 
 <!-- caraousel -->
 
-<!--<div id="myCarousel" class="carousel slide" data-ride="carousel">-->
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
 	<!-- Indicators -->
-<!--	<ol class="carousel-indicators">-->
-<!--		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>-->
-<!--		<li data-target="#myCarousel" data-slide-to="1"></li>-->
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
 
-<!--	</ol>-->
+	</ol>
 
-<!-- Wrapper for slides -->
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner" role="listbox">
+		<div class="item active">
+			<a href="login.php">
+				<img src="img/widesample.png" alt="sample">
+			</a>
+		</div>
+		<div class="item">
+			<a href="signup.php">
+				<img src="img/widesample1.png" alt="sample2">
+			</a>
+		</div>
 
-<!--	<div class="carousel-inner" role="listbox">-->
-<!--		<div class="item active">-->
-<!--			<a href="login.php">-->
-<!--				<img src="img/widesample.png" alt="sample">-->
-<!--			</a>-->
-<!--		</div>-->
-<!--		<div class="item">-->
-<!--			<a href="signup.php">-->
-<!--				<img src="img/widesample1.png" alt="sample2">-->
-<!--			</a>-->
-<!--		</div>-->
-<!---->
-<!--	</div>-->
-<!---->
-<!-- Controls -->
+	</div>
 
-<!--	<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">-->
-<!--		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>-->
-<!--		<span class="sr-only">Previous</span>-->
-<!--	</a>-->
-<!--	<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">-->
-<!--		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>-->
-<!--		<span class="sr-only">Next</span>-->
-<!--	</a>-->
-<!--</div>-->
+	<!-- Controls -->
+	<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	</a>
+</div>
 
 
-<div class="jumbotron" id="content">
+<div id="content">
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<script type="text/javascript" src="js/counter.js"></script>
 	<script type="text/javascript" src="js/shcart.js"></script>
 
-	
+	<h2> Flowers</h2>
 	<div class="row">   
 		
 		<div id="productslistforhome">
