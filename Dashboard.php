91a149a1ee8383ff45177198229859861d04aaa7
Viktor <purple_goth@bk.ru>
2016-04-07 09:52:51 -0700

    reverting
    141

--- Dashboard.php
+++ Dashboard.php
@@ -3,7 +3,6 @@ session_start();
 require 'db.php';
 include 'head.php';
 ?>
-<<<<<<< HEAD
 
 <div class="jumbotron">
     <h1>Dashboard</h1>
@@ -176,74 +175,6 @@ include 'head.php';
         $("#addNewUser").submit(function (e) {
             e.preventDefault();
 
-=======
-<h2>Dashboard</h2>
-<h3>Customers</h3>
-
-
-<div id="content"  class="Absolute-Center is-Responsive">
-
-        <table id="users" style="width: 500" >
-            <thead>
-            <tr>
-                <th class="id_header" style="width: 5%"><span>id</span></th>
-                <th class="name_header" style="width: 25%"><span>Login</span></th>
-                <th class="email_header" style="width: 20%"><span>Email</span></th>
-                <th class="password_header" style="width: 20%"><span>Password</span></th>
-                <th class="delete_header" style="width: 10%"><span>Delete</span></th>
-                <th class="delete_header" style="width: 10%"><span>Update</span></th>
-            </tr>
-            </thead>
-
-            <tbody id='userrows'>
-            <!-- THIS SECTION WILL BE REPLACED BY SERVER GENERATED ROWS -->
-            <tr>
-                <td class="id"><span>aaa</span></td>
-                <td class="name"><span>aaa</span></td>
-                <td class="email"><span>bbb</span></td>
-                <td class="password"><span>uniqueloginname</span></td>
-                <td><input id="uniqueloginname" class="delete" type="button" value="Delete"/></td>
-            </tr>
-            </tbody>
-        </table>
-        <form id="addNewUser">
-            <input id="addLogin" type="text" placeholder="Login" required="required"/>
-            <input id="addEmail" type="text" placeholder="Email" required="required"/>
-            <input id="addPw" type="text" placeholder="Pw" required="required"/>
-            <input id="submitNewUser" type="submit" value="Add"/>
-        </form>
-    </div>
-
-
-
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script>
-
-    $(document).ready(function() {
-
-        function loadUsers() {
-            $.ajax({
-                url: "./users.php",
-                type: "GET",
-                dataType: 'html',
-                success: function(returnedData) {
-                    //console.log("cart checkout response: ", returnedData);
-                    $("#userrows").html(returnedData);
-                    //window.location.href = "user-editor.html";
-                },
-                error: function(jqXHR, textStatus, errorThrown) {
-                    console.log(jqXHR.statusText, textStatus);
-                }
-            });
-        }
-
-        loadUsers();
-
-
-        $("#addNewUser").submit(function(e) {
-            e.preventDefault();
-
->>>>>>> origin/master
             // get values from form
             var Login = $("#addLogin").val();
             var Email = $("#addEmail").val();
@@ -255,7 +186,6 @@ include 'head.php';
                 url: "users.php",
                 type: "POST",
                 dataType: "JSON",
-<<<<<<< HEAD
                 data: {
                     action: "add", newLogin: Login,
                     newEmail: Email, newPw: Pw
@@ -265,15 +195,6 @@ include 'head.php';
                     console.log(returnedData);
                 },
                 error: function (jqXHR, textStatus, errorThrown) {
-=======
-                data: {action: "add", newLogin: Login,
-                    newEmail: Email, newPw: Pw},
-                success: function(returnedData) {
-                    loadUsers();
-                    console.log(returnedData);
-                },
-                error: function(jqXHR, textStatus, errorThrown) {
->>>>>>> origin/master
                     $("#p1").text(jqXHR.statusText);
                 }
 
@@ -281,11 +202,7 @@ include 'head.php';
 
         });
 
-<<<<<<< HEAD
         $('#users').on('click', '.delete', function () {
-=======
-        $('#users').on('click', '.delete', function() {
->>>>>>> origin/master
             var loginValue = this.getAttribute("id");
             loginValue = loginValue.replace("d-", "");
 
@@ -293,71 +210,43 @@ include 'head.php';
                 url: "./users.php",
                 type: "POST",
                 dataType: 'json',
-<<<<<<< HEAD
                 data: {action: "delete", username: loginValue},
                 success: function (returnedData) {
-=======
-                data: {action: "delete", name: loginValue},
-                success: function(returnedData) {
->>>>>>> origin/master
                     loadUsers();
 
                     //window.location.href = "user-editor.html";
                 },
-<<<<<<< HEAD
                 error: function (jqXHR, textStatus, errorThrown) {
-=======
-                error: function(jqXHR, textStatus, errorThrown) {
->>>>>>> origin/master
                     console.log(jqXHR.statusText, textStatus);
                 }
             });
 
         });
 
-<<<<<<< HEAD
         $('#users').on('click', '.update', function () {
             var loginValue = this.getAttribute("id");
             //var firstName = $(this).val(
             loginValue = loginValue.replace("u-", "");
             var editedFirstName = $(this).parent().parent().find(".Login span").text();
             //console.log(loginValue, editedFirstName);
-=======
-        $('#users').on('click', '.update', function() {
-            var loginValue = this.getAttribute("id");
-            //var firstName = $(this).val(
-            loginValue = loginValue.replace("u-", "");
-            var editedLogin = $(this).parent().parent().find(".name span").text();
-            //console.log(loginValue, editedLogin);
->>>>>>> origin/master
 
             $.ajax({
                 url: "./users.php",
                 type: "POST",
                 dataType: 'json',
-<<<<<<< HEAD
                 data: {action: "update", username: loginValue, newFirstName: editedFirstName},
                 success: function (returnedData) {
-=======
-                data: {action: "update", id: loginValue, newLogin: editedLogin},
-                success: function(returnedData) {
->>>>>>> origin/master
                     loadUsers();
 
                     //window.location.href = "user-editor.html";
                 },
-<<<<<<< HEAD
                 error: function (jqXHR, textStatus, errorThrown) {
-=======
-                error: function(jqXHR, textStatus, errorThrown) {
->>>>>>> origin/master
                     console.log(jqXHR.statusText, textStatus);
                 }
             });
 
         });
 
-<<<<<<< HEAD
 
         // http://stackoverflow.com/questions/11882693/change-table-cell-from-span-to-input-on-click
         $('#users').on('click', 'span', function () {
@@ -391,52 +280,9 @@ include 'head.php';
                 });
             }
         });
-=======
 
-        // http://stackoverflow.com/questions/11882693/change-table-cell-from-span-to-input-on-click
-        $('#users').on('click', 'span', function() {
-
-            var $td = $(this).parent();
-            var $input = null;
-
-            var val = $(this).html();
-            //name changing field
-            if($td.attr('class') === 'name') {
-                //var val = $(this).html();
-                $td.html('<input type="text" value="' + val + '"/>');
-                $input = $td.find('input');
-                $input.focus();
-                $input.select();
-            }
-            //Email changing field
-
-            if($input != null) {
-
-                $input.on('blur', function() {
-                    $(this).parent().html('<span>' + $(this).val() + '</span>');
-                });
-
-                $input.keyup(function(e) {
-                    if(e.which == 13) {
-                        $(this).parent().html('<span>' + $(this).val() + '</span>');
-                    } else if(e.which == 27) {
-                        // escape key, means user canceled operation
-                        $(this).parent().html('<span>' + val + '</span>');
-                    }
-                });
-            }
-        });
-
-
->>>>>>> origin/master
 
     });
-
-<<<<<<< HEAD
-    });
-=======
-</script>
->>>>>>> origin/master
 
 </script>
 
--- productsForHome.php
+++ productsForHome.php
@@ -5,7 +5,7 @@ loadScripts();
 
 $data = array("status" => "not set!");
 
-if (Utils::isGET() && !Utils::isPOST()) {
+if (Utils::isGET()) {
     $pm = new ProductManager();
     $rows = $pm->listProducts();
 
@@ -14,14 +14,12 @@ if (Utils::isGET() && !Utils::isPOST()) {
         $html = "<tr><td>There are no results.</td></tr>";
     } else {
 
-       // foreach ($rows as $row) {
-            for($i = 0; $i < 4; $i++){
-
-            $path = $rows[$i]['path'];
-            $sku = $rows[$i]['SKU'];
-            $price = $rows[$i]['item_price'];
-            $desc = $rows[$i]['description'];
-
+        foreach ($rows as $row) {
+            $path = $row['path'];
+            $sku = $row['SKU'];
+            $price = $row['item_price'];
+            $desc = $row['description'];
+				
 
             $html .= "		<div class='col-md-3 thumbnailwrapper ' >
 			<a href='#' class='thumbnail'>
@@ -39,6 +37,7 @@ if (Utils::isGET() && !Utils::isPOST()) {
 			</div>
 		</div>";
 
+
         }
 
     }
@@ -47,45 +46,10 @@ if (Utils::isGET() && !Utils::isPOST()) {
     return;
 
 } else {
+    $data = array("status" => "error", "msg" => "Only GET allowed.");
 
-        $pm = new ProductManager();
-        $rows = $pm->listProducts();
-
-        $html = "";
-        if (!$rows || count($rows) == 0) {
-            $html = "<tr><td>There are no results.</td></tr>";
-        } else {
-
-            foreach ($rows as $row) {
-                $path = $row['path'];
-                $sku = $row['SKU'];
-                $price = $row['item_price'];
-                $desc = $row['description'];
-
-
-                $html .= "		<div class='col-md-3 thumbnailwrapper ' >
-			<a href='#' class='thumbnail'>
-			<div class='panel panel-default'>
-				 <img src=$path alt=$desc style='width:150px;height:150px'>
-				 <br/>
-				<p data-sku-desc='$sku'>$desc</p>
-				<!--
-				<p>model# $sku</p>
-				-->
-				<strong>$</strong><strong data-sku-price='$sku'>$price</strong>
-				
-			</a>			
-			<input data-sku-add='$sku' type='button' value='Add to cart' class='btn btn-primary btn-block'/>
-			</div>
-		</div>";
-
-            }
-
-        }
-
-        echo $html;
-        return;
 }
+
 echo json_encode($data, JSON_FORCE_OBJECT);
 
 
